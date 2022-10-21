b126d43599e76d6a5b5d892b8164eec433e7842a
smartdev58 <wingun1123@gmail.com>
2022-10-21 13:29:28 -0700

    devise path changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   resources :answers
   resources :questions
   devise_for :users,
+             path: '',
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout',
+               registrations: 'registrations'
+             },
              controllers: {
                sessions: 'api/v1/users/sessions',
                registrations: 'api/v1/users/registrations'
