9ffba28797f9b7ca5449bf46a5f6b004dcb00d83
smartdev58 <wingun1123@gmail.com>
2022-10-21 16:12:53 -0700

    fix

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -310,5 +310,12 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
   config.jwt do |jwt|
     jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
+    jwt.dispatch_requests = [
+        ['POST', %r{^/api/v1/login$}]
+      ]
+      jwt.revocation_requests = [
+        ['DELETE', %r{^/api/v1/logout$}]
+      ]
+    jwt.expiration_time = 5.minutes.to_i
   end
 end
